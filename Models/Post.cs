45f6e85680662d94985912bbefedbfd2996ba02f
migmaram <miguel.mzrz.93@gmail.com>
2025-03-04 22:13:37 -0600

    Initial commit

---
+++ Models/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleBloggingAPI.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Category { get; set; }
+        public List<string> Tags { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
